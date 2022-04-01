a4a212d3213dfc315d56cb0b5b0368c050f7f759
Chun <caochun@gmail.com>
2022-04-01 17:20:15 +0800

    update

--- src/main/java/com/example/tugas/controller/HomeController.java
+++ src/main/java/com/example/tugas/controller/HomeController.java
@@ -5,11 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-
     @RequestMapping(value = "/")
     public String index() {
         return "index";
     }
-
 }
