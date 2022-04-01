6209e125ae1c16b4233648e04b331a4516297ac1
Chun <caochun@gmail.com>
2022-04-01 23:26:22 +0800

    refactor to multi-module

--- tugas-web/src/main/java/com/example/tugas/web/TugasApplication.java
+++ tugas-web/src/main/java/com/example/tugas/web/TugasApplication.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.tugas")
+@ComponentScan({"com.example.tugas"})
+@EntityScan("com.example.tugas.employee")
+@EnableJpaRepositories("com.example.tugas.employee")
 public class TugasApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
